d0bb937c4bb5a897219af667a2cf787f1503b9c1
shiqiang <shiqiang@zhito.com>
2023-01-11 10:39:50 +0800

    常规提交5

--- test12_schema.py
+++ test12_schema.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# 暂未测试通过
+# 暂未测试通过，先忽略吧
 import xmlschema
 from pprint import pprint
 
@@ -7,7 +7,7 @@ xml_file="test12.xml"
 
 schema_file = open('test12.xsd')
 my_schema = xmlschema.XMLSchema(schema_file, build=False)
-_ = my_schema.include_schema('test12_gender.xsd')
+# _ = my_schema.include_schema('test12_gender.xsd')
 my_schema.build()
 
 print("XSD file is: [\033[1;35m"+'test12.xsd'+' '+'test12_gender.xsd'+"\033[0m]. XML file is: [\033[1;35m"+xml_file+"\033[0m].")
@@ -17,4 +17,5 @@ if ret2 is None:
   print("\033[1;32mcorrect\033[0m")
 else:
   print("\033[1;31mcorrect\033[0m") # 如果validate失败，它会抛异常，所以事实上不会执行这行代码
+
   
